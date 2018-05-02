cbf8e144139a7a967cf31abe1478aa570f67bcad
Samuel Batissou <samuelbatissou@gmail.com>
2018-05-02 20:07:38 +0200

    Added mash page

--- controller/index.js
+++ controller/index.js
@@ -2,24 +2,39 @@ const model = require('../model/model');
 
 
 exports.score_page = (req, res) => {
-    console.log(model.CatModel.find({}).sort('score').exec((err,cats) => {
-        if (err){
+    model.CatModel.find({}).sort('score').exec((err, cats) => {
+        if (err) {
             console.error("Error fetching kittens on the db", err);
-        }else {
-            res.render('scores', {cats:cats})
+        } else {
+            res.render('scores', {cats: cats})
         }
-    }));
+    });
 };
 
-
-model.CatModel.find({}, (err, cats) => {
+exports.mash_page = (req, res) => {
+    model.CatModel.count((err, count) => {
         if (err) {
-            console.error("Error :",err);
-        }
-        else {
+            console.error(err)
+        } else {
+            let rnd2;
+            let rnd;
+            do {
+                rnd = Math.floor(Math.random() * count);
+                rnd2 = Math.floor(Math.random() * count);
+            } while (rnd === rnd2);
+            let pro_cats = [];
+            pro_cats[0] = model.CatModel.findOne().skip(rnd).exec();
+            pro_cats[1] = model.CatModel.findOne().skip(rnd2).exec();
 
+            Promise.all(pro_cats).then(
+                cats => {
+                    res.render('mash', {cats: cats})
+                },
+                err => {
+                    console.error("Error during cats promising ", err)
+                })
         }
-    }
-)
-;
+    })
+};
+
 
--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,16 @@ const router = express.Router();
 const controller = require('../controller/index');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    res.render('index', {title: 'Express'});
 });
 
-router.get('/scores', function (req,res,next) {
-    controller.score_page(req,res);
+router.get('/scores', (req, res, next) => {
+    controller.score_page(req, res);
+});
+
+router.get('/mash', (req, res, next) => {
+    controller.mash_page(req,res)
 });
 
 module.exports = router;
